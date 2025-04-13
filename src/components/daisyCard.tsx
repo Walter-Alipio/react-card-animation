809d77fa0b10b3d4b193b00f0d37de95c4cccc9c
Walter <walteralipio@outlook.com>
2025-04-13 07:07:57 -0300

    set dark mode colors

--- src/components/daisyCard.tsx
+++ src/components/daisyCard.tsx
@@ -22,13 +22,13 @@ const DaisyCard = () => {
             <AnimatedDiv
                 ref={ref}
                 style={springs}
-                className="bg-white rounded-lg m-5 p-10 min-h-[45vh] w-sm md:w-md flex flex-col justify-around"
+                className="bg-white dark:bg-gray-700 rounded-lg m-5 p-10 min-h-[45vh] w-sm md:w-md flex flex-col justify-around"
                 {...({} as any)}
             >
-                <h6 className="font-bold text-4xl text-gray-700 font-sans">Consulte suas dívidas<br /> agora mesmo!</h6>
+                <h6 className="font-bold text-4xl text-gray-700 dark:text-gray-50 font-sans">Consulte suas dívidas<br /> agora mesmo!</h6>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <fieldset className="mb-7">
-                        <legend className="text-md text-gray-600 mb-3">Busque pelo seu CPF ou CNPJ</legend>
+                        <legend className="text-md text-gray-600 dark:text-gray-50 mb-3">Busque pelo seu CPF ou CNPJ</legend>
                         <input 
                             type="text" 
                             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
@@ -37,8 +37,8 @@ const DaisyCard = () => {
                         {errors.cpfCnpj && <span className='text-red-800'>Campo obrigatório</span>}
                     </fieldset>
                     <fieldset className="mb-7">
-                        <input type="checkbox" name="tmUso" id="tmUso" className='bg-white' />
-                        <span className="text-gray-600 ml-3">Li e concordo com os <a href="#" className="text-blue-400 font-semibold hover:link">Termos de Uso.</a></span>
+                        <input type="checkbox" name="tmUso" id="tmUso" />
+                        <span className="text-gray-600 dark:text-gray-50 ml-3">Li e concordo com os <a href="#" className="text-blue-400 font-semibold hover:link">Termos de Uso.</a></span>
                     </fieldset>
                     <button 
                         type="submit" 
